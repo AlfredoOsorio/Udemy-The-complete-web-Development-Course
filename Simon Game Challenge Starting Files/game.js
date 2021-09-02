a086463956bd29db783749cdf5e137215ae1e8d2
Alfredo Osorio <alfredo2710181@gmail.com>
2021-09-02 13:47:22 +0200

    Update game.js
    commit step 3 of boss challenge 2

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,12 +1,52 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
-const gamePattern
+const gamePattern = [];
+
+const audioByColor = ["redAudio", "blueAudio", "greenAudio", "yellowAudio"];
+
+var redAudio = new Audio("sounds/red.mp3");
+var blueAudio = new Audio("sounds/blue.mp3");
+var greenAudio = new Audio("sounds/green.mp3");
+var yellowAudio = new Audio("sounds/yellow.mp3");
+
+function blinkButton(buttonColor) {
+    $("#" + buttonColor).fadeOut(200).fadeIn(200);
+}
+
+function playAudioButton(buttonColor) {
+
+    switch (buttonColor) {
+        case "red":
+            redAudio.play();
+            break;
+    
+        case "blue":
+            blueAudio.play();
+            break;
+    
+        case "green":
+            greenAudio.play();
+            break;
+    
+        case "yellow":
+            yellowAudio.play();
+            break;
+    
+        default:
+            break;
+    }
+}
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-
-    return randomNumber;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    blinkButton(randomChosenColour);
+    playAudioButton(randomChosenColour);
 }
 
-var randomChosenColour = buttonColours[nextSequence];
-
-gamePattern.push(randomChosenColour);
+nextSequence();
+nextSequence();
+nextSequence();
+nextSequence();
+nextSequence();
+nextSequence();
