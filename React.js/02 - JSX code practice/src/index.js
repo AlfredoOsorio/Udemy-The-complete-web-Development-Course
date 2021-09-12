af70cfce8d4bbed1f2027d15d860c69cd94ff501
Alfredo Osorio <alfredo2710181@gmail.com>
2021-09-12 19:51:38 +0200

    Update index.js
    Completed challenge

--- React.js/02 - JSX code practice/src/index.js
+++ React.js/02 - JSX code practice/src/index.js
@@ -2,3 +2,19 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Hobbies</h1>
+    <ul>
+      <li>Magic: The Gathering</li>
+      <li>Videogames</li>
+      <li>Listening to music</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
