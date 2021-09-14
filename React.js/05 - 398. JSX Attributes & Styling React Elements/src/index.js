fd60a8f42a461bd936513cd8475229bd2a7162b0
Alfredo Osorio <alfredo2710181@gmail.com>
2021-09-14 22:30:05 +0200

    reorganizing on last update
    no major changes

--- React.js/05 - 398. JSX Attributes & Styling React Elements/src/index.js
+++ React.js/05 - 398. JSX Attributes & Styling React Elements/src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const pizzaImg = "https://i-ticketing.iwos.com/256x256-th/products/190/products_190_49.jpg";
+const pizzaImg =
+  "https://i-ticketing.iwos.com/256x256-th/products/190/products_190_49.jpg";
 
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">
       My Favourite Foods
     </h1>
-    <img
-      className="size100x100"
-      alt="Pizza"
-      src={pizzaImg}
-    ></img>
+    <img className="size100x100" alt="Pizza" src={pizzaImg}></img>
     <img
       className="size100x100"
       alt="Ramen"
