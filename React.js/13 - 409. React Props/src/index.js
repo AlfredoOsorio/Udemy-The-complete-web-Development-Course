785d46ee56c284c0f4e438337cfb044c69ffd0b2
Alfredo Osorio <alfredo2710181@gmail.com>
2021-10-02 15:30:59 +0200

    updating 409. React Props
    adding some updates to this so it has more consistency

--- React.js/13 - 409. React Props/src/index.js
+++ React.js/13 - 409. React Props/src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Card from "./components/Card";
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt={props.alt} />
-      <p>{props.phoneNumber}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
