12a6a08ea1bd5e000b16333409cb2bcedce830a1
Alfredo Osorio <alfredo2710181@gmail.com>
2021-10-04 22:48:09 +0200

    Final changes to the React Props Practice

--- React.js/14 - 410. React Props Practice/src/components/App.jsx
+++ React.js/14 - 410. React Props Practice/src/components/App.jsx
@@ -13,6 +13,20 @@ function App() {
         phoneNumber={contacts[0].phone}
         email={contacts[0].email}
       />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        imageAlt={contacts[1].imageAlt}
+        phoneNumber={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        imageAlt={contacts[2].imageAlt}
+        phoneNumber={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
