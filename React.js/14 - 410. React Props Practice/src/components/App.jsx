5b9a029856911d69bf70ef836325f2aad074fd6a
Alfredo Osorio <alfredo2710181@gmail.com>
2021-10-04 21:03:09 +0200

    update the card component with contacts info
    export contacts from contacts.js into App.jsx, an refactor the Card component with the contact information

--- React.js/14 - 410. React Props Practice/src/components/App.jsx
+++ React.js/14 - 410. React Props Practice/src/components/App.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        imageAlt="avatar_img"
-        phoneNumber="+123 456 789"
-        email="b@beyonce.com"
-      />
-      <Card
-        name="Beyonce"
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        imageAlt="avatar_img"
-        phoneNumber="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        imageAlt={contacts[0].imageAlt}
+        phoneNumber={contacts[0].phone}
+        email={contacts[0].email}
       />
     </div>
   );
--- React.js/14 - 410. React Props Practice/src/components/Card.jsx
+++ React.js/14 - 410. React Props Practice/src/components/Card.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Card(props) {
   return (
     <div>
-      <div>
-        <div>
+      <div className="card">
+        <div className="top">
           <h2>{props.name}</h2>
-          <img src={props.img} alt={props.imageAlt} />
+          <img className="circle-img" src={props.img} alt={props.imageAlt} />
         </div>
-        <div ClassName="bottom">
+        <div className="bottom">
           <p>{props.phoneNumber}</p>
           <p>{props.email}</p>
         </div>
@@ -18,21 +18,3 @@ function Card(props) {
 }
 
 export default Card;
-
-/*
-<div>
-  <div className="card">
-    <div className="top">
-      <h2>Beyonce</h2>
-      <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        alt="avatar_img"
-      />
-    </div>
-    <div className="bottom">
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
-    </div>
-  </div>
-</div>
-*/
