bac6483620c51132ef9d48974fb8adde97f5f8ab
Alfredo Osorio <alfredo2710181@gmail.com>
2021-10-05 23:56:20 +0200

    Full completion of the 411. React DevTools

--- React.js/15 - 411. React DevTools/src/components/App.jsx
+++ React.js/15 - 411. React DevTools/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
+      <Avatar imgSrc="https://i.guim.co.uk/img/media/6088d89032f8673c3473567a91157080840a7bb8/413_955_2808_1685/master/2808.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=412cc526a799b2d3fff991129cb8f030" />
       <h1 className="heading">My Contacts</h1>
 
       <Card
