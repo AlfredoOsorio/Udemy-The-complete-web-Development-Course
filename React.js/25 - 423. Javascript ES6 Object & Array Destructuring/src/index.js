4aa0c07d8484775ea25ac111015d8317297aeeb8
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-07 17:32:01 +0100

    Completion of class 423
    423. Javascript ES6 Object & Array Destructuring

--- React.js/25 - 423. Javascript ES6 Object & Array Destructuring/src/index.js
+++ React.js/25 - 423. Javascript ES6 Object & Array Destructuring/src/index.js
@@ -1,23 +1,46 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+
+//tesla
+const {
+  coloursByPopularity: teslaColours,
+  speedStats: teslaSpeedStats
+} = tesla;
+
+const { topSpeed: teslaTopSpeed } = teslaSpeedStats;
+const [teslaTopColour] = teslaColours;
+
+//honda
+const {
+  coloursByPopularity: hondaColours,
+  speedStats: hondaSpeedStats
+} = honda;
+
+const { topSpeed: hondaTopSpeed } = hondaSpeedStats;
+const [hondaTopColour] = hondaColours;
+
+//render
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
