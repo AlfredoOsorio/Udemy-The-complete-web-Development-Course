dd25719d88aa103bca96aa3e11a966356dc8014b
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-12 23:24:04 +0100

    final steps of class 425. React Forms

--- React.js/27 - 425. React Forms/src/components/App.jsx
+++ React.js/27 - 425. React Forms/src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [heading, setHeading] = useState("Hello");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading("Hello " + name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{heading}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
