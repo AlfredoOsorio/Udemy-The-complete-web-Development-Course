14eca493a70e9aa00352edfb72b0ea2dd7ddc729
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-12 23:22:58 +0100

    commit starting files of 425
    425. React Forms

--- React.js/27 - 425. React Forms/src/components/App.jsx
+++ React.js/27 - 425. React Forms/src/components/App.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [heading, setHeading] = useState("Hello");
-
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleClick() {
-    setHeading("Hello " + name);
-  }
-
   return (
     <div className="container">
-      <h1>{heading}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-        value={name}
-      />
-      <button onClick={handleClick}>Submit</button>
+      <h1>Hello </h1>
+      <input type="text" placeholder="What's your name?" />
+      <button>Submit</button>
     </div>
   );
 }
