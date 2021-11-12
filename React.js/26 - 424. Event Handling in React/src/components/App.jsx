c4e24a12689f8eb11970523046f459510300f4d4
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-12 22:53:27 +0100

    completion of class 424
    424. Event Handling in React

--- React.js/26 - 424. Event Handling in React/src/components/App.jsx
+++ React.js/26 - 424. Event Handling in React/src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("Hello");
+  const [isMousedOver, setMousedOver] = useState(false);
+
+  function handleClick() {
+    setHeading("Submitted");
+  }
+
+  function turnButtonBlack() {
+    setMousedOver(true);
+  }
+
+  function turnButtonWhite() {
+    setMousedOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onMouseOver={turnButtonBlack}
+        onMouseOut={turnButtonWhite}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
