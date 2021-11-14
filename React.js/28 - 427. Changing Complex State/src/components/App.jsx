2e2ff83c4b6fa319a4b731e4a0c5e0d7ffe6895a
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-14 21:42:10 +0100

    Completion of first mini challenge of class 427
    427. Changing Complex State

--- React.js/28 - 427. Changing Complex State/src/components/App.jsx
+++ React.js/28 - 427. Changing Complex State/src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {name} {lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleNameChange}
+          name="fName"
+          placeholder="First Name"
+          value={name}
+        />
+        <input
+          onChange={handleLastNameChange}
+          name="lName"
+          placeholder="Last Name"
+          value={lastName}
+        />
         <button>Submit</button>
       </form>
     </div>
