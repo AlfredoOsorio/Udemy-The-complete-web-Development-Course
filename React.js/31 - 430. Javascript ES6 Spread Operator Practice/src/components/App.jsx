b4f28ce65108dc8faa4dd31153e2b70450f30985
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-18 22:10:15 +0100

    Update App.jsx

--- React.js/31 - 430. Javascript ES6 Spread Operator Practice/src/components/App.jsx
+++ React.js/31 - 430. Javascript ES6 Spread Operator Practice/src/components/App.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+    setItem(value);
+  }
+
+  function additem() {
+    setItems((items) => [...items, item]);
+  }
+
+  console.log(items);
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          name="item"
+          onChange={handleInputChange}
+          placeholder=""
+          value={item}
+        />
+        <button onClick={additem}>
           <span>Add</span>
         </button>
       </div>
