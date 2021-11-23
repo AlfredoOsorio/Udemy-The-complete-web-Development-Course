ca810f4afd6703ad9e1890be76398458caba3bc3
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-23 19:43:41 +0100

    completion of class 432
    432. Managing a Component Tree Practice
    
    I did it differently than what the course said though

--- React.js/33 - 432. Managing a Component Tree Practice/src/components/InputArea.jsx
+++ React.js/33 - 432. Managing a Component Tree Practice/src/components/InputArea.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
   return (
     <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <input onChange={props.newItem} type="text" value={props.newitemValue} />
+      <button onClick={props.addNewItem}>
         <span>Add</span>
       </button>
     </div>
