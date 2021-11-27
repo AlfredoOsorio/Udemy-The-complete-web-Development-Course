a9d6edaf903c073bcec42fdfa363d94bab886b31
Alfredo Osorio <alfredo2710181@gmail.com>
2021-11-27 23:47:06 +0100

    Completion of class 434
    434. React Dependencies & Styling the Keeper App

--- React.js/35 - 434. React Dependencies & Styling the Keeper App/src/components/CreateArea.jsx
+++ React.js/35 - 434. React Dependencies & Styling the Keeper App/src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -9,12 +14,16 @@ function CreateArea(props) {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
         [name]: value
       };
     });
+  }
+
+  function expandNoteOnClick() {
+    setIsExpanded(true);
   }
 
   function submitNote(event) {
@@ -30,19 +39,27 @@ function CreateArea(props) {
     <div>
       <form className="create-note">
         <input
+          type={isExpanded ? "" : "hidden"}
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <button onClick={submitNote}>Add</button>
+        <div>
+          <textarea
+            name="content"
+            onChange={handleChange}
+            onClick={expandNoteOnClick}
+            value={note.content}
+            placeholder="Take a note..."
+            rows={isExpanded ? 1 : 1}
+          />
+        </div>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
