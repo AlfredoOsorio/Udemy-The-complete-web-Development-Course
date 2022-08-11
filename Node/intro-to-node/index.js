0ecd66202c9056e4a4167f13c1c495079500d946
Alfredo Osorio <alfredo2710181@gmail.com>
2022-08-11 22:11:26 +0200

    adding the super heroes/villains modules

--- Node/intro-to-node/index.js
+++ Node/intro-to-node/index.js
@@ -1,1 +1,8 @@
-const fs = require("fs");
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySupeHeroeName = superheroes.random();
+var mySupeVillainName = supervillains.random();
+
+console.log(mySupeHeroeName);
+console.log(mySupeVillainName);
